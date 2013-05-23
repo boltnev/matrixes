d4427619d521a62208573979eff525eba07874b9
Ilya Boltnev <ilya@boltnev.com>
2013-05-23 14:05:59 +0400

    Benchmarks for gauss method and multiplication added

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,8 +1,7 @@
 #include "benchmark.h"
-
-#define MAT_SIZE 20000
+#define MAT_SIZE 1000
  
-int randomize(){
+void randomize(){
 	double seconds;
     time_t s_time, f_time;
 	time(&s_time);
@@ -15,11 +14,53 @@ int randomize(){
 	time(&f_time);
 	seconds = difftime(f_time, s_time);
 	
-    printf ("randomize test, %d X %d : %.f seconds \n", MAT_SIZE, MAT_SIZE, seconds);	  
+    printf ("randomize, %d X %d : %.f seconds \n", MAT_SIZE, MAT_SIZE, seconds);	  
+}
+
+void multiplication(){
+	double seconds;
+    time_t s_time, f_time;
+	
+	Matrix<double> a(MAT_SIZE, MAT_SIZE);
+	Matrix<double> b(MAT_SIZE, MAT_SIZE);
+	a.randomize();
+	b.randomize();
+	
+	time(&s_time);
+	
+	//	
+	Matrix<double> c = a * b;
+	//
+
+	time(&f_time);
+	seconds = difftime(f_time, s_time);
+	
+    printf ("Simple multiplication, %d X %d : %.f seconds \n", MAT_SIZE, MAT_SIZE, seconds);	  
+}
+
+void gauss(){
+	double seconds;
+    time_t s_time, f_time;
+	
+	Matrix<double> a(MAT_SIZE, MAT_SIZE + 1);
+	a.randomize();
+
+	time(&s_time);
+	
+	//	
+	a.gauss();
+	//
+
+	time(&f_time);
+	seconds = difftime(f_time, s_time);
+	
+    printf ("Simple gauss solution, %d X %d : %.f seconds \n", MAT_SIZE, MAT_SIZE, seconds);	  
 }
 
 
 int benchmark(){
 	randomize();
-		
+	multiplication();
+	gauss();
+	return 0;
 }
